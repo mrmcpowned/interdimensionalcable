8d66b1cdd98fe9d1b4324548a362512d2d4bd051
Christopher Rodriguez <chrismr3000@gmail.com>
2016-06-11 15:07:08 -0400

    Implementing a better video getter

--- js/get-video.js
+++ js/get-video.js
@@ -1,13 +1,11 @@
 var videoObj = {};
 var videoInfo = {};
 var videoList = [];
-var redditData = {};
 var redditHash;
 var channelNames = ["1", "2", "TWO", "3", "4", "42", "1337", "5", "6", "117", "7", "A113", "8", "9", "10", "🐐", "101", "C137", "👌😂", "🍌", "🍆", "20", "30", "40", "50", "60", "69", "70", "80", "90", "100", "C132", "35C", "J19ζ7"];
 var audioQuotes = ["sexsells", "improv", "relax", "billmurray"];
 var tvState = 0;
 //Since I'm depending on reddit's api for getting curated videos, I have to prepare for occasion where reddit would be down 
-var redditState = 0;
 var loginState = 0;
 var muteState = 0;
 var menuState = 0;
@@ -15,8 +13,6 @@ var duckState = 0;
 var animationState = 0;
 var tvAudioLevel = 50;
 var channelDelayState = 0;
-var currentVideo;
-var currentVideoID;
 var player;
 var bgv;
 var bga;
@@ -68,12 +64,10 @@ function turnOnOffTV() {
         tvState = 1;
         $('body').toggleClass('tv-on');
         $('body').removeClass('tv-off');
-        if (redditState) {
-            currentVideo = videoList[currentVideoID];
             player = new YT.Player('yt-iframe', {
                 width: 1280,
                 height: 720,
-                videoId: currentVideo,
+                videoId: get_video()[0],
                 playerVars: {
                     'autoplay': 1,
                     'controls': 0,
@@ -88,7 +82,6 @@ function turnOnOffTV() {
                     'onError': onPlayerError
                 }
             });
-        }
         cha.play();
         if (muteState) {
             $('.container').addClass('mute');
@@ -104,9 +97,7 @@ function turnOnOffTV() {
             console.log('tv menu reset');
             menuToggle();
         }
-        if (redditState) {
             player.destroy();
-        }
         bga.play();
         channelDelayState = 0;
         tvState = 0;
@@ -160,19 +151,13 @@ function onPlayerStateChange(event) {
 }
 
 function nextChannel() {
-    if (tvState && redditState) {
+    if (tvState) {
         if (Math.random() <= 0.1) {
             setTimeout(playQuote(), 200);
         }
         clearTimeout(autoChannelDelay);
         console.log("Timeout Removed on channel change!");
-        currentVideoID++;
-        currentVideo = videoList[currentVideoID];
-        player.loadVideoById(currentVideo);
-        if (currentVideoID == videoList.length) {
-            getVideos();
-            console.log("Video list refreshed!");
-        }
+        player.loadVideoById(get_video()[0]);
         cha.play();
         console.log("Channel Changed!");
         changeChannelName();
@@ -185,7 +170,7 @@ function nextChannel() {
 }
 
 function volumeUp(direction) {
-    if (tvState && redditState && !duckState) {
+    if (tvState && !duckState) {
         if (tvAudioLevel !== 100) {
             tvAudioLevel += 10;
             $('.volume').addClass("reset");
@@ -204,7 +189,7 @@ function volumeUp(direction) {
 }
 
 function volumeDown() {
-    if (tvState && redditState && !duckState) {
+    if (tvState && !duckState) {
         if (tvAudioLevel === 10) {
             tvAudioLevel -= 10;
             mute();
@@ -222,7 +207,7 @@ function volumeDown() {
 }
 
 function mute() {
-    if (tvState && redditState) {
+    if (tvState) {
         if (!muteState) {
             player.mute();
             $('.container').addClass('mute');
@@ -255,7 +240,7 @@ function menuToggle() {
 }
 
 function openVideo() {
-    if (tvState && redditState) {
+    if (tvState) {
         window.open(player.getVideoUrl(), '_blank');
     }
 }
@@ -299,84 +284,85 @@ function playQuote() {
     }
 }
 
-function checkLoggedIn() {
-    var jqxhr = $.ajax({
-        url: "https://api.reddit.com/api/me.json"
-    }).done(function (response) {
-        if (typeof response.data.modhash !== "undefined") {
-            loginState = 1;
-            redditHash = response.data.modhash;
-        } else {
-            loginState = 0;
-        }
-    });
+
+if (!Array.prototype.randomElement) {
+    Array.prototype.randomElement = function () {
+        return this[Math.floor(Math.random() * this.length)]
+    };
 }
 
-function sortVideoData() {
-    //    var tcode = new RegExp(/(?:(?:http|https):\/\/(?:youtu\.be|youtube\.com|.*\.youtube\.com)\/)(?:.*?)(\?t|&t)/);
-    var tcode = new RegExp(/((?:\?|;|&)t=)/);
-    //    var videoIdReg = new RegExp(/(?:(?:http|https):\/\/(?:youtu\.be|youtube\.com|.*\.youtube\.com)\/)(?:watch\?v=(.*?)&|watch\?v=(.*)|(.*)\?|(.*))/);
-    /**
-    Ok, so like, my RegEx sucks. Here I've basically tried to gather the video ID properly from all these URLs.
-    With the regex ontop, I'm finding URLs wit ha timestamp so I can later ignore then in the loop below. I know this can most likely be done with RegEx itself, but since I'm looping through
-    */
-    var videoIdReg = new RegExp(/(?:(?:http|https):\/\/(?:youtu\.be|youtube\.com|.*\.youtube\.com)\/)(?:watch\?(?:.*?)v=(.*?)&|watch\?(?:.*?)v=(.*)|watch\?v=(.*?)&|watch\?v=(.*)|(.*)\?|(.*))/);
-    //    var videoIdReg = new RegExp(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/);
-    for (var i = 0; i < videoObj.data.children.length; i++) {
-        var submittedUrl = videoObj.data.children[i].data.url;
-        if (tcode.test(submittedUrl)) {
-            continue;
-        } else {
-            var capturedUrl = videoIdReg.exec(submittedUrl);
-            var capturedId;
-            for (var ii = 1; ii < capturedUrl.length; ii++) {
-                if (typeof capturedUrl[ii] !== "undefined") {
-                    capturedId = capturedUrl[ii];
-                    break;
+if (!Array.prototype.randomPop) {
+    Array.prototype.randomPop = function () {
+        var index = Math.floor(Math.random() * this.length);
+        return this.splice(index, 1);
+    };
+}
+
+var get_video = (function () {
+
+    var played = [];
+    var videos = [];
+
+    var youtube_video_regex = new RegExp(/^.*(youtu.be\/|v\/|embed\/|watch\?|youtube.com\/user\/[^#]*#([^\/]*?\/)*)\??v?=?([^#\&\?]*).*/);
+
+    var get_api_call = function (time, sort) {
+        return `https://www.reddit.com/r/InterdimensionalCable/search.json?q=site%3Ayoutube.com&restrict_sr=on&sort=${sort}&t=${time}&limit=50`;
+    };
+
+    var add_youtube_url = function (youtube_video_url) {
+        // Check if the URL is for youtube
+        if (!youtube_video_regex.test(youtube_video_url)) {
+            return false;
+        }
+        // Check to see if the entier video is being linked.
+        // If a certain index is being linked, ignore the video.
+        if (youtube_video_url.indexOf("&t=") != -1) {
+            return false;
+        }
+        var groups = youtube_video_regex.exec(youtube_video_url);
+        // TODO: Trim video id?
+        var video_id = groups[3]; // 3rd group is the video id.
+        if (played.indexOf(video_id) != -1 || videos.indexOf(video_id) != -1) {
+            return false;
+        }
+        videos.push(video_id);
+        return true;
+    };
+
+    var load_more = function () {
+        var time = ["week", "month", "year", "all"].randomElement();
+        var sort = ["relevance", "hot", "top", "new", "comments"].randomElement();
+        var url = get_api_call(time, sort);
+        $.getJSON(url, function (data) {
+            data.data.children.forEach(function (child) {
+                if (add_youtube_url(child.data.url)) {
+                    console.log("Added " + child.data.url);
+                } else {
+                    console.log("Ignored " + child.data.url);
                 }
-            }
-            videoList.push(capturedId);
-            //            videoInfo[capturedId] = {
-            //                title: videoObj.data.children[i].data.media.oembed.title,
-            //                description: videoObj.data.children[i].data.media.oembed.description
-            //            };
-            videoInfo[capturedId] = videoObj.data.children[i].data;
-        }
-    }
-    videoList = shuffle(videoList);
-}
-
-function getVideos() {
-    videoObj = {};
-    videoInfo = {};
-    videoList = [];
-    currentVideoID = 0;
-    var timeOptions = ["week", "month", "year", "all"];
-    var sortOptions = ["relevance", "hot", "top", "new", "comments"];
-    var redditURL = "https://www.reddit.com/r/InterdimensionalCable/search.json?q=site%3Ayoutube.com+OR+site%3Ayoutu.be+AND+self%3Ano&restrict_sr=on&sort=" + sortOptions[Math.floor(Math.random() * sortOptions.length)] + "&t=" + timeOptions[Math.floor(Math.random() * timeOptions.length)] + "&limit=50";
-    var jqxhr = $.ajax({
-            url: redditURL
-        })
-        .done(function (data) {
-            redditState = 1;
-            videoObj = data;
-            sortVideoData();
-            $('.container').removeClass('offline');
-        }).fail(function () {
-            redditState = 0;
-            $('.container').addClass('offline');
+            });
         });
-}
+    };
+
+    load_more();
+
+    return function () {
+        if (videos.length < 5) {
+            if (videos.length == 0) {
+                return null;
+            }
+            load_more();
+        }
+        var item = videos.randomPop();
+        played.push(item);
+        return item;
+    };
+})();
 
 $(function () {
     bgv = document.getElementById("rick-bg");
     bga = document.getElementById("off-audio");
     cha = document.getElementById("switch-audio");
     qa = document.getElementById("quote-player");
-    getVideos();
-    $.ajax({
-        url: "https://www.reddit.com/api/me.json"
-    }).done(function (data) {
-        redditData = data;
-    });
+
 });
